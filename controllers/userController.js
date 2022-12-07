507b44b8b5b2356533fb35abe58f98ff6f3f4e64
Sahaj Kedia <64389239+sahajkedia@users.noreply.github.com>
2022-12-07 16:29:05 +0530

    updates user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,11 +21,36 @@ exports.getUser = (req, res) => {
   });
 };
 
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: "error",
-    message: "This route is not yet defined!!",
-  });
+exports.createUser = async (req, res) => {
+  const { name, email, phone, photo, role, password, passwordConfirm } =
+    req.body;
+  console.log(req.body);
+
+  try {
+    const newUser = await User.create({
+      name,
+      email,
+      phone,
+      photo,
+      role,
+      password,
+      passwordConfirm,
+    });
+    newUser.password = undefined;
+    await newUser.save();
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "error",
+      message: "This route is not yet defined!!",
+    });
+  }
 };
 
 exports.updateUser = (req, res) => {
