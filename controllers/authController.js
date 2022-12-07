bb22d38f492e594369f3edaf4d0e3a694308fb8e
dreadwing5 <sachinspyk@outlook.com>
2022-12-07 15:18:52 +0530

    Fix model bug

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
+const User = require("../models/User");
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 const sendEmail = require("../utils/email");
